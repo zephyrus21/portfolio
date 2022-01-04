eb899e100efe776face10eea1e79bcd3516dd6ec
zephyrus21 <pandey.piyus@gmail.com>
2022-01-05 00:06:57 +0530

    still setting up sc

--- components/Header/HeaderStyles.tsx
+++ components/Header/HeaderStyles.tsx
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div``;
+
+export const SubHeading = styled.h2`
+  font-family: var(--secondary-font);
+  font-size: 1.7rem;
+  color: var(--primary-color);
+`;
+
+export const Heading = styled.h1`
+  font-family: var(--primary-font);
+  font-size: 6.85rem;
+  color: var(--primary-text-color);
+`;
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,14 @@
-import { Wrapper } from "./HeaderStyles";
+import { Heading, SubHeading, Wrapper } from "./HeaderStyles";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = ({}) => {
-  return <Wrapper>Header</Wrapper>;
+  return (
+    <Wrapper>
+      <SubHeading>Hi, my name is</SubHeading>
+      <Heading>Piyush Pandey</Heading>
+    </Wrapper>
+  );
 };
 
 export default Header;

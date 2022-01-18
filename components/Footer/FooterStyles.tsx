ccac1d28be96a8a04d922ec149abe0ca166b9c00
zephyrus21 <pandey.piyus@gmail.com>
2022-01-18 21:02:03 +0530

    added footer component

--- components/Footer/FooterStyles.tsx
+++ components/Footer/FooterStyles.tsx
@@ -1,11 +1,28 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  padding: 12rem 16rem;
+  padding: 2.2rem 1rem;
   background: var(--bg-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   @media only screen and (max-width: 900px) {
   }
 
   @media only screen and (max-width: 500px) {
   }
+`;
+
+export const Content = styled.div`
+  font-size: 1.2rem;
+  color: var(--secondary-text-color);
+  font-family: var(--secondary-font);
+`;
+
+export const Social = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin-bottom: 1.4rem;
 `;
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,57 @@
-import { Wrapper } from "./FooterStyles";
+import Link from "next/link";
+import { useContext } from "react";
+import { GlobalContext } from "../../context/Globalcontext";
+import { StyledLink } from "../SideElements/SideElementsStyles";
+import { Content, Social, Wrapper } from "./FooterStyles";
+import {
+  GithubOutline,
+  LinkedinOutline,
+  TwitterOutline,
+} from "@styled-icons/evaicons-outline";
+import { Instagram } from "@styled-icons/fa-brands/Instagram";
 
 interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = ({}) => {
-  return <Wrapper>Footer</Wrapper>;
+  const { width }: any = useContext(GlobalContext);
+
+  return (
+    <Wrapper>
+      {width ? null : (
+        <Social>
+          <StyledLink>
+            <Link href='https://github.com/zephyrus21' passHref>
+              <a target='_blank'>
+                <GithubOutline />
+              </a>
+            </Link>
+          </StyledLink>
+          <StyledLink>
+            <Link href='https://twitter.com/zephyrus_io' passHref>
+              <a target='_blank'>
+                <TwitterOutline />
+              </a>
+            </Link>
+          </StyledLink>
+          <StyledLink>
+            <Link href='https://www.instagram.com/zephyrus.io/' passHref>
+              <a target='_blank'>
+                <Instagram />
+              </a>
+            </Link>
+          </StyledLink>
+          <StyledLink>
+            <Link href='https://www.linkedin.com/in/zephyrus21' passHref>
+              <a target='_blank'>
+                <LinkedinOutline />
+              </a>
+            </Link>
+          </StyledLink>
+        </Social>
+      )}
+      <Content>Designed & Developed by Piyush</Content>
+    </Wrapper>
+  );
 };
 
 export default Footer;

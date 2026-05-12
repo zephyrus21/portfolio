3e6ce98ce15bc3989f95f300678f5fcb2c345716
zephyrus21 <pandey.piyus@gmail.com>
2026-05-13 01:05:29 +0530

    feat: add Vercel adapter, update Astro version, and implement caching headers for blog and gallery pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+});
